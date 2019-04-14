35c93620ba22cb29ae72d08a3f5717db0cdb01a0
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-14 18:24:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,14 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5],
+  [6,7,8],
+  [0,4,5],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
+  
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -7,6 +17,8 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-  
+
+
+
 
   
